4040fbb68542513e5ce0b59857a28f49c127b2b6
imdbsd <budisuryadarma0407@gmail.com>
2019-07-16 20:32:37 +0700

    remove getEvolutionChain request

--- poke-fe/src/requests/index.js
+++ poke-fe/src/requests/index.js
@@ -1,6 +1,5 @@
 import getPokemons from './getPokemons'
 import getPokemon from './getPokemon'
 import getPokemonSpecies from './getPokemonSpecies'
-import getEvolutionChain from './getEvolutionChain'
 
-export { getPokemons, getPokemon, getPokemonSpecies, getEvolutionChain }
+export { getPokemons, getPokemon, getPokemonSpecies }
