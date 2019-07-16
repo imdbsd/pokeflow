dc8827b811b6c6fbde68a2fef460e62658d61a6a
imdbsd <budisuryadarma0407@gmail.com>
2019-07-16 20:33:21 +0700

    remove getPokemonSpecies

--- poke-fe/src/components/SpeciesCard/SpeciesCard.js
+++ poke-fe/src/components/SpeciesCard/SpeciesCard.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import DescriptionCard from '../DescriptionCard'
-import { getPokemonSpecies } from '../../requests'
 import { SpeciesBody, SpeciesDiv } from './styles'
 
 function SpeciesCard(props) {
--- poke-fe/src/requests/index.js
+++ poke-fe/src/requests/index.js
@@ -1,5 +1,4 @@
 import getPokemons from './getPokemons'
 import getPokemon from './getPokemon'
-import getPokemonSpecies from './getPokemonSpecies'
 
-export { getPokemons, getPokemon, getPokemonSpecies }
+export { getPokemons, getPokemon }
