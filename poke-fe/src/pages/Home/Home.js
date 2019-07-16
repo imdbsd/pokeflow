d32ae12a69cb67fce0ebb87c3404fadebc18d8b2
imdbsd <budisuryadarma0407@gmail.com>
2019-07-16 20:39:49 +0700

    change home to funcion component

--- poke-fe/src/pages/Home/Home.js
+++ poke-fe/src/pages/Home/Home.js
@@ -1,4 +1,4 @@
-import React, { Fragment, Component } from 'react'
+import React, { Fragment } from 'react'
 import { Query } from 'react-apollo'
 import { gql } from 'apollo-boost'
 import { Header, PokemonCard, Paginations } from '../../components'
@@ -18,48 +18,46 @@ const GET_POKEMON_QUERY = gql`
   }
 `
 
-class Home extends Component {
-  render() {
-    return (
-      <Fragment>
-        <Header />
-        <AppContainer>
-          <Query query={GET_POKEMON_QUERY} variables={{ url: '' }}>
-            {({ loading, error, data, fetchMore }) => {
-              if (loading) return 'Loading...'
-              if (error) return `Error! ${error.message}`
-              console.log(data)
-              return (
-                <Fragment>
-                  {data.getPokemons.pokemons.map((pokemon, index) => (
-                    <PokemonCard key={pokemon.id} {...pokemon} />
-                  ))}
-                  <Paginations
-                    handlePagination={url =>
-                      fetchMore({
-                        variables: {
-                          url: url
-                        },
-                        updateQuery: (prev, { fetchMoreResult }) => {
-                          if (!fetchMoreResult) return prev
-                          console.log({ fetchMoreResult })
-                          return Object.assign({}, prev, {
-                            getPokemons: { ...fetchMoreResult.getPokemons }
-                          })
-                        }
-                      })
-                    }
-                    next={data.getPokemons.next}
-                    previous={data.getPokemons.previous}
-                  />
-                </Fragment>
-              )
-            }}
-          </Query>
-        </AppContainer>
-      </Fragment>
-    )
-  }
+function Home() {
+  return (
+    <Fragment>
+      <Header />
+      <AppContainer>
+        <Query query={GET_POKEMON_QUERY} variables={{ url: '' }}>
+          {({ loading, error, data, fetchMore }) => {
+            if (loading) return 'Loading...'
+            if (error) return `Error! ${error.message}`
+            console.log(data)
+            return (
+              <Fragment>
+                {data.getPokemons.pokemons.map((pokemon, index) => (
+                  <PokemonCard key={pokemon.id} {...pokemon} />
+                ))}
+                <Paginations
+                  handlePagination={url =>
+                    fetchMore({
+                      variables: {
+                        url: url
+                      },
+                      updateQuery: (prev, { fetchMoreResult }) => {
+                        if (!fetchMoreResult) return prev
+                        console.log({ fetchMoreResult })
+                        return Object.assign({}, prev, {
+                          getPokemons: { ...fetchMoreResult.getPokemons }
+                        })
+                      }
+                    })
+                  }
+                  next={data.getPokemons.next}
+                  previous={data.getPokemons.previous}
+                />
+              </Fragment>
+            )
+          }}
+        </Query>
+      </AppContainer>
+    </Fragment>
+  )
 }
 
 export default Home
